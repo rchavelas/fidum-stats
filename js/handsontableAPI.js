577042662921763637aec23215dcd6c4ceec4009
Ricardo Chavelas <ricardo.chavelas.m@gmail.com>
2021-08-22 21:22:15 -0500

    updated extraction from handsontable

--- js/handsontableAPI.js
+++ js/handsontableAPI.js
@@ -1,13 +1,15 @@
 const container = document.getElementById("spreadsheet");
 
 const settings = {
-    startCols: 50,
-    startRows: 200,
+    contextMenu: true,
+    startCols: 30,
+    startRows: 70,
     colWidths: 75,
     height: "100%",
     width: "100%",
     rowHeaders: true,
     colHeaders: true,
+    manualColumnResize: true,
     licenseKey: 'non-commercial-and-evaluation',
 }
 
--- js/main.js
+++ js/main.js
@@ -1,10 +1,55 @@
 "use strict"
 
-console.log(hot.getSourceData())
+// Main functionalities of Fidum Online Statistical Software
 
-// Function to clean handsontable GetSourceData()
-// Returns an array of arrays with only the columns with any value
-// cleanHandosontableSourceData
+// A) Helper functions
 
-// Function to print data to fidumOutput (worksheetData)
+// FUNCTION to clean handsontable GetSourceData()
+// Returns an array of arrays with only the columns and rows with any value
+const cleanHandosontableSourceData = function(arrOfArr){
+    // Clear empty rows
+    let arrOfArrUsedRows = arrOfArr.filter(arr => {
+        return !arr.every(value => (value === null || value === ""))
+    })
+    
+    // Clear emtpy cells
+    let arrOfArrUserInput;
+    if(arrOfArrUsedRows.length>0){
+        // Create an array as a flag to filter columns (usedColumns)
+        let len = arrOfArrUsedRows[0].length
+        let usedColumns = new Array(len).fill(false)
+        // Modify the state of usedColumns based on the contents of the arrOfArrUsedRows
+        arrOfArrUsedRows.forEach(arr => {
+            arr.forEach((value,index)=>{
+                if(!(value === null || value === "")){usedColumns[index] = true}
+            });
+        });
+        // Remove all columns in arrofArrUsedRows with all null values
+        arrOfArrUserInput = arrOfArrUsedRows.map(arr => {
+            return arr.filter((value,index)=>usedColumns[index]);
+        })
+    } else {
+        arrOfArrUserInput = [...arrOfArrUsedRows]
+    }
+
+    // Print to otuput area
+    return(arrOfArrUserInput)
+}
+
+// FUNCTION to print data to fidumOutput (worksheetData)
 // printWorksheetData
+
+// FUNCTION to chage the order of the arr of Arr from rowwise to columnwise
+
+// B) Event Listeners
+
+// Print output data to output 
+// TODO... summarize function based on type of data
+document.getElementById("getHotData").addEventListener("click",function(){
+    let hotDataArray = hot.getSourceDataArray()
+    let prettyPrintArray = JSON.stringify(cleanHandosontableSourceData(hotDataArray),null,2) 
+    let p = document.createElement("p");
+    p.innerHTML = prettyPrintArray
+    document.getElementById("output").appendChild(p)
+    console.log(prettyPrintArray)
+})
