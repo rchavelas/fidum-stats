794e84dbf8195b3f4f7c9f015ffd537171ff20b1
Ricardo Chavelas <ricardo.chavelas.m@gmail.com>
2021-09-04 09:00:59 -0500

    column header integration and renaming

--- js/handsontableAPI.js
+++ js/handsontableAPI.js
@@ -1,9 +1,12 @@
+'use strict'
+
+// Initialization of handsontable whitin APP
 const container = document.getElementById("spreadsheet");
 
 const settings = {
     contextMenu: true,
-    startCols: 30,
-    startRows: 70,
+    startCols: 26,
+    startRows: 100,
     colWidths: 75,
     height: "100%",
     width: "100%",
@@ -15,3 +18,89 @@ const settings = {
 
 const hot = new Handsontable(container,settings);
 
+// FUNCTION to clean handsontable GetSourceData() from hot API
+// Returns an array of arrays with only the columns and rows with any value
+const cleanHandosontableSourceData = function(arrOfArr, headersArray){
+    // Clear empty rows
+    let arrOfArrUsedRows = arrOfArr.filter(arr => {
+        return !arr.every(value => (value === null || value === ""))
+    })
+    // Create placeholders for input and headers
+    let arrOfArrUserInput;
+    let arrOfUsedHeaders;
+    // Clear emtpy cells
+    if(arrOfArrUsedRows.length>0){
+        // Create an array as a flag to filter columns (usedColumns)
+        let len = arrOfArrUsedRows[0].length
+        let usedColumns = new Array(len).fill(false)
+        // Modify the state of usedColumns based on the contents of the arrOfArrUsedRows
+        arrOfArrUsedRows.forEach(arr => {
+            arr.forEach((value,index)=>{
+                if(!(value === null || value === "")){usedColumns[index] = true}
+            });
+        });
+        // Remove all columns in arrofArrUsedRows with all null values
+        arrOfArrUserInput = arrOfArrUsedRows.map(arr => {
+            return arr.filter((value,index)=>usedColumns[index]);
+        })
+        // Remove headers that contain no data
+        arrOfUsedHeaders = headersArray.filter((col,index) => usedColumns[index]);
+        
+    } else {
+        arrOfArrUserInput = [...arrOfArrUsedRows]
+        arrOfUsedHeaders = []
+    }
+    // Create placeholder to output data
+    let userInput = {
+        "headers": arrOfUsedHeaders,
+        "arrOfArr": arrOfArrUserInput
+    }
+    // return clean data
+    return(userInput)
+}
+
+// FUNCTION to validate contents of columns
+
+// FUNCTION to change colheader 
+const updateHOTColHeaders = function(currentColumnName,newColumnName){
+    console.log("Changing column name") 
+    let newHotColHeaders = [...hot.getColHeader()];
+    // Validate if newColumnName already exists
+    if(newHotColHeaders.indexOf(newColumnName)>=0){
+        return "Column name already exists";
+    } else {
+        // Replace if currentColumnName exists
+        let currentHeaderIndex = newHotColHeaders.indexOf(currentColumnName);
+        // Deny if new value already exists
+        if(currentHeaderIndex >= 0){
+            let replacedColName = newHotColHeaders.splice(currentHeaderIndex,1,newColumnName)
+            hot.updateSettings({colHeaders: newHotColHeaders});
+            return `Replaced ${replacedColName} with ${newColumnName}`
+        } else {
+            return "Missing column";
+        }
+    }
+}
+
+
+
+// Fake data to bind for testing
+// Array of rows
+const testData = [
+    ['', 'Tesla', 'Nissan', 'Toyota', 'Honda', 'Mazda', 'Ford'],
+    ['2017', 10, 11, 12, 13, 15, 16],
+    ['2018', 9, 11, 12, 13, 15, 16],
+    ['2019', 8, 11, 12, 13, 15, 16],
+    ['2020', 7, 11, 12, 13, 15, 16],
+    ['2021', 6, 11, 12, 13, 15, 16]
+];
+
+const testData2 = [[1,1,1],[1,2,1],[-1,-1,-1.5],[-1,-1,-1.5]]
+
+// Input data to hot
+let testDataOutput = [...testData2]
+for (let row = 0; row < testDataOutput.length; row++) {
+    for (let column = 0; column < testDataOutput[row].length; column++) {
+        hot.setDataAtCell(row, column, testDataOutput[row][column]);
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,129 +1,100 @@
 "use strict"
-
-// Main functionalities of Fidum Online Statistical Software
-
-// A) Helper functions
-
-// FUNCTION to clean handsontable GetSourceData()
-// Returns an array of arrays with only the columns and rows with any value
-const cleanHandosontableSourceData = function(arrOfArr){
-    // Clear empty rows
-    let arrOfArrUsedRows = arrOfArr.filter(arr => {
-        return !arr.every(value => (value === null || value === ""))
-    })
+// Execute only when page has finished loading
+document.addEventListener('DOMContentLoaded', function() {
     
-    // Clear emtpy cells
-    let arrOfArrUserInput;
-    if(arrOfArrUsedRows.length>0){
-        // Create an array as a flag to filter columns (usedColumns)
-        let len = arrOfArrUsedRows[0].length
-        let usedColumns = new Array(len).fill(false)
-        // Modify the state of usedColumns based on the contents of the arrOfArrUsedRows
-        arrOfArrUsedRows.forEach(arr => {
-            arr.forEach((value,index)=>{
-                if(!(value === null || value === "")){usedColumns[index] = true}
-            });
-        });
-        // Remove all columns in arrofArrUsedRows with all null values
-        arrOfArrUserInput = arrOfArrUsedRows.map(arr => {
-            return arr.filter((value,index)=>usedColumns[index]);
-        })
-    } else {
-        arrOfArrUserInput = [...arrOfArrUsedRows]
-    }
-
-    // Print to otuput area
-    return(arrOfArrUserInput)
-}
-
-// FUNCTION to print data to fidumOutput (worksheetData)
-// printWorksheetData
-
-// FUNCTION to chage the order of the arr of Arr from rowwise to columnwise
-
-// B) Event Listeners
-let outputDocumentID = document.getElementById("output")
-// Print output data to output 
-// TODO... summarize function based on type of data
-document.getElementById("getHotData").addEventListener("click",function(){
-    // Log operation
-    console.log("Printing data")
-    // Get data from Handsontable
-    let hotDataArray = hot.getSourceDataArray()
-    // Print title and annotations in output region
-    let p = document.createElement("p");
-    p.innerHTML = `Printing data`
-    p.classList.add("resultOutputTitle")
-    outputDocumentID.appendChild(p)  
-    // Prepare data to print
-    let prettyPrintArray = JSON.stringify(cleanHandosontableSourceData(hotDataArray),null,2) 
-    let p2 = document.createElement("p");
-    p2.innerHTML = prettyPrintArray
-    outputDocumentID.appendChild(p2)
-    // Run descriptiveStatistics routine
-    let desctStatsObjOutput = JSON.stringify(descriptiveStatistics(),null,2)
-    let p3 = document.createElement("p");
-    p3.innerHTML = desctStatsObjOutput
-    outputDocumentID.appendChild(p3)
-    // scroll to bottom
-    p2.scrollIntoView({behaviour:"smooth"});
-})
-
-//Run kmeans algorithm from kMeans.js and print it to Output
-document.getElementById("computeKmeans").addEventListener("click",function(){
-    // Log operation
-    console.log("Runing kmeans")
-    // Get input options
-    let useNaiveShardingKmeans = false;
-    if( document.getElementById("kMeansInitializacion").value === "kMeansNaiveShardingInit"){
-        useNaiveShardingKmeans = true
-        console.log("using naive sharding")
-    }
-    // Get data from Handsontable
-    let dataKm = cleanHandosontableSourceData(hot.getSourceDataArray())
-    // Clean and validate data for kmeans algorithm
-    // PENDING
-    // Run kmeans algorithm
-    let k = Number(document.getElementById("kMeansClusters").value)
-    let result = kmeans(dataKm, k, useNaiveShardingKmeans)
-    // Print title and annotations in output region
-    let p = document.createElement("p");
-    p.innerHTML = `Kmeans clustering with ${k} clusters`
-    p.classList.add("resultOutputTitle")
-    outputDocumentID.appendChild(p)  
-    // Print result to output region
-    result.clusters.forEach(cluster => {
-        let clusterInfo = JSON.stringify(cluster.centroid,null,2) 
+    // Main functionalities of Fidum Online Statistical Software
+    
+    // A) Helper functions
+    
+    // B) Event Listeners
+    let outputDocumentID = document.getElementById("output")
+    // Print output data to output 
+    // TODO... summarize function based on type of data
+    document.getElementById("getHotData").addEventListener("click",function(){
+        // Log operation
+        console.log("Printing data")
+        // Get data from Handsontable
+        let hotDataArray = hot.getSourceDataArray();
+        let hotHeadersArray = hot.getColHeader();
+        // Print title and annotations in output region
         let p = document.createElement("p");
-        p.innerHTML = clusterInfo
-        outputDocumentID.appendChild(p)
+        p.innerHTML = `Printing data`
+        p.classList.add("resultOutputTitle")
+        outputDocumentID.appendChild(p)  
+        // Prepare data to print
+        let prettyPrintArray = JSON.stringify(cleanHandosontableSourceData(hotDataArray,hotHeadersArray),null,2) 
+        let p2 = document.createElement("p");
+        p2.innerHTML = prettyPrintArray
+        outputDocumentID.appendChild(p2)
+        // Run descriptiveStatistics routine
+        //let desctStatsObjOutput = JSON.stringify(descriptiveStatistics(),null,2)
+        //let p3 = document.createElement("p");
+        //p3.innerHTML = desctStatsObjOutput
+        // outputDocumentID.appendChild(p3)
         // scroll to bottom
-        p.scrollIntoView({behaviour:"smooth"});
+        p2.scrollIntoView({behaviour:"smooth"});
     });
-})
-
-// C) Layout functinalities
-// Popupu modal implementation
-// SEE https://medium.com/@GistCoding/simple-popup-modal-with-vanilla-javascript-a14515ec630b
-const modalTriggers = document.querySelectorAll('.popup-trigger')
-const modalCloseTrigger = document.querySelector('.popup-modal__close')
-const bodyBlackout = document.querySelector('.body-blackout')
-modalTriggers.forEach(trigger => {
-  trigger.addEventListener('click', () => {
-    const { popupTrigger } = trigger.dataset
-    const popupModal = document.querySelector(`[data-popup-modal="${popupTrigger}"]`)
-    popupModal.classList.add('is--visible')
-    bodyBlackout.classList.add('is-blacked-out')
     
-    popupModal.querySelector('.popup-modal__close').addEventListener('click', () => {
-       popupModal.classList.remove('is--visible')
-       bodyBlackout.classList.remove('is-blacked-out')
+    //Run kmeans algorithm from kMeans.js and print it to Output
+    document.getElementById("computeKmeans").addEventListener("click",function(){
+        // Log operation
+        console.log("Runing kmeans")
+        // Get input options
+        let useNaiveShardingKmeans = false;
+        if( document.getElementById("kMeansInitializacion").value === "kMeansNaiveShardingInit"){
+            useNaiveShardingKmeans = true
+            console.log("using naive sharding")
+        }
+        // Get data from Handsontable
+        // Get data from Handsontable
+        let hotDataArray = hot.getSourceDataArray();
+        let hotHeadersArray = hot.getColHeader();
+        let dataKm = cleanHandosontableSourceData(hotDataArray,hotHeadersArray).arrOfArr
+        // Clean and validate data for kmeans algorithm
+        // PENDING
+        // Run kmeans algorithm
+        let k = Number(document.getElementById("kMeansClusters").value)
+        let result = kmeans(dataKm, k, useNaiveShardingKmeans)
+        // Print title and annotations in output region
+        let p = document.createElement("p");
+        p.innerHTML = `Kmeans clustering with ${k} clusters`
+        p.classList.add("resultOutputTitle")
+        outputDocumentID.appendChild(p)  
+        // Print result to output region
+        result.clusters.forEach(cluster => {
+            let clusterInfo = JSON.stringify(cluster.centroid,null,2) 
+            let p = document.createElement("p");
+            p.innerHTML = clusterInfo
+            outputDocumentID.appendChild(p)
+            // scroll to bottom
+            p.scrollIntoView({behaviour:"smooth"});
+        });
     })
     
-    popupModal.querySelector('.popup-modal__run').addEventListener('click', () => {
-        popupModal.classList.remove('is--visible')
-        bodyBlackout.classList.remove('is-blacked-out')
-     })
-
-  })
-})
+    // C) Layout functinalities
+    // Popupu modal implementation
+    // SEE https://medium.com/@GistCoding/simple-popup-modal-with-vanilla-javascript-a14515ec630b
+    const modalTriggers = document.querySelectorAll('.popup-trigger')
+    const modalCloseTrigger = document.querySelector('.popup-modal__close')
+    const bodyBlackout = document.querySelector('.body-blackout')
+    modalTriggers.forEach(trigger => {
+        trigger.addEventListener('click', () => {
+            const { popupTrigger } = trigger.dataset
+            const popupModal = document.querySelector(`[data-popup-modal="${popupTrigger}"]`)
+            popupModal.classList.add('is--visible')
+            bodyBlackout.classList.add('is-blacked-out')
+            
+            popupModal.querySelector('.popup-modal__close').addEventListener('click', () => {
+                popupModal.classList.remove('is--visible')
+                bodyBlackout.classList.remove('is-blacked-out')
+            })
+            
+            popupModal.querySelector('.popup-modal__run').addEventListener('click', () => {
+                popupModal.classList.remove('is--visible')
+                bodyBlackout.classList.remove('is-blacked-out')
+            })
+            
+        })
+    })
+    
+}); // end of: DOMContentLoaded
