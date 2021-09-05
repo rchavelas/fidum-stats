ae858a1da6898df32d56f04255117f088e0f1f20
Ricardo Chavelas <ricardo.chavelas.m@gmail.com>
2021-09-04 20:17:20 -0500

    improved descriptive stats behaviour and var,sd,n computing

--- js/descStat.js
+++ js/descStat.js
@@ -3,7 +3,26 @@
 // Functions to provide basic statistics to columns declared in spreadsheet
 // Mainly powered by functionalities in jstat https://github.com/jstat/jstat
 
-// Get data from handsontable
+// Helper fuctions
+// Variance (https://www.tutorialspoint.com/calculating-variance-for-an-array-of-numbers-in-javascript)
+function varFn(arr){
+    if(!arr.length){
+        return 0
+    }
+    // compute mean
+    var sum = arr.reduce((acc,val) => acc + val);
+    var num = arr.length;
+    var mean = sum/num;
+    // Compute var
+    var variance = 0;
+    arr.forEach(val => {
+        variance += ((val-mean)*(val-mean))
+    })
+    variance /= (num-1)
+    return variance
+}
+
+// Compute selected stats 
 function descriptiveStatistics(cleanHotDataColwise, selectedCols,selectedStats){
     // arguments: prettyPrintArray, selectedColumns [1,3,4,...], stats: [mean, mode, sd, var,...]
     let statsVals = cleanHotDataColwise
@@ -11,10 +30,29 @@ function descriptiveStatistics(cleanHotDataColwise, selectedCols,selectedStats){
     .map(col => {
         let statsObj = {colName: col.colName}
         // Compute selected statistics
+        // n obs
+        if(selectedStats.includes("n")){
+            let colData = col.colArr;
+            let n = colData.length
+            statsObj.n = n
+        }
+        // Mean
         if(selectedStats.includes("mean")){
             let colData = col.colArr;
             let mean = colData.reduce((acc,val)=>acc+val)/colData.length
-            statsObj.mean = mean
+            statsObj.mean = roundFn(mean, defaultDecimalPlaces)
+        }
+        // variance (sample)
+        if(selectedStats.includes("var")){
+            let colData = col.colArr;
+            let variance = varFn(colData)
+            statsObj.variance = roundFn(variance, defaultDecimalPlaces)
+        }
+        // standard deviation (sample)
+        if(selectedStats.includes("sd")){
+            let colData = col.colArr;
+            let sd = Math.sqrt(varFn(colData))
+            statsObj.sd = roundFn(sd, defaultDecimalPlaces)
         }
         // Return array of stat values
         return statsObj 
--- js/main.js
+++ js/main.js
@@ -1,104 +1,115 @@
 "use strict"
 // Execute only when page has finished loading
-document.addEventListener('DOMContentLoaded', function() {
-    
-    // Main functionalities of Fidum Online Statistical Software
-    
-    // A) Helper functions
-    
-    // B) Event Listeners
-    let outputDocumentID = document.getElementById("output")
-    // Print output data to output 
-    // TODO... summarize function based on type of data
-    document.getElementById("getHotData").addEventListener("click",function(){
-        // Log operation
-        console.log("Printing data")
-        // Get data from Handsontable
-        let hotDataArray = hot.getSourceDataArray();
-        let hotHeadersArray = hot.getColHeader();
-        // Print title and annotations in output region
+//document.addEventListener('DOMContentLoaded', function() {
+
+// Main functionalities of Fidum Online Statistical Software
+
+// 0) App config
+let defaultDecimalPlaces = 3;
+
+// A) Helper functions
+// A.1) Round decimal places (https://medium.com/swlh/how-to-round-to-a-certain-number-of-decimal-places-in-javascript-ed74c471c1b8)
+var roundFn = function(number, decimalPlaces){
+    if(number>=0){
+        return Number(Math.round(number + "e" + decimalPlaces) + "e-" + decimalPlaces)  
+    } else {
+        return -Number(Math.round(number*(-1) + "e" + decimalPlaces) + "e-" + decimalPlaces)
+    }
+}
+
+// B) Event Listeners
+let outputDocumentID = document.getElementById("output")
+// Print output data to output 
+// TODO... summarize function based on type of data
+document.getElementById("getHotData").addEventListener("click",function(){
+    // Log operation
+    console.log("Printing data")
+    // Get data from Handsontable
+    let hotDataArray = hot.getSourceDataArray();
+    let hotHeadersArray = hot.getColHeader();
+    // Print title and annotations in output region
+    let p = document.createElement("p");
+    p.innerHTML = `Printing data`
+    p.classList.add("resultOutputTitle")
+    outputDocumentID.appendChild(p)  
+    // Prepare data to print
+    let cleanHotData = cleanHandosontableSourceData(hotDataArray,hotHeadersArray)
+    let cleanHotDataColwise = columnWiseHandsontableCleanData(cleanHotData)
+    let prettyPrintArray = JSON.stringify(cleanHotDataColwise,null,2)  
+    let p2 = document.createElement("p");
+    p2.innerHTML = prettyPrintArray
+    //outputDocumentID.appendChild(p2)
+    // Run descriptiveStatistics routine
+    let selectedCols = ["s_length", "s_width","p_length","p_width"]
+    let selectedStats = ["mean", "var","n","sd"]
+    let desctStatsObjOutput = JSON.stringify(descriptiveStatistics(cleanHotDataColwise,selectedCols,selectedStats),null,2)
+    let p3 = document.createElement("p");
+    p3.innerHTML = desctStatsObjOutput
+    outputDocumentID.appendChild(p3)
+    // scroll to bottom
+    p2.scrollIntoView({behaviour:"smooth"});
+});
+
+//Run kmeans algorithm from kMeans.js and print it to Output
+document.getElementById("computeKmeans").addEventListener("click",function(){
+    // Log operation
+    console.log("Runing kmeans")
+    // Get input options
+    let useNaiveShardingKmeans = false;
+    if( document.getElementById("kMeansInitializacion").value === "kMeansNaiveShardingInit"){
+        useNaiveShardingKmeans = true
+        console.log("using naive sharding")
+    }
+    // Get data from Handsontable
+    // Get data from Handsontable
+    let hotDataArray = hot.getSourceDataArray();
+    let hotHeadersArray = hot.getColHeader();
+    let dataKm = cleanHandosontableSourceData(hotDataArray,hotHeadersArray).arrOfArr
+    // Clean and validate data for kmeans algorithm
+    // PENDING
+    // Run kmeans algorithm
+    let k = Number(document.getElementById("kMeansClusters").value)
+    let result = kmeans(dataKm, k, useNaiveShardingKmeans)
+    // Print title and annotations in output region
+    let p = document.createElement("p");
+    p.innerHTML = `Kmeans clustering with ${k} clusters`
+    p.classList.add("resultOutputTitle")
+    outputDocumentID.appendChild(p)  
+    // Print result to output region
+    result.clusters.forEach(cluster => {
+        let clusterInfo = JSON.stringify(cluster.centroid,null,2) 
         let p = document.createElement("p");
-        p.innerHTML = `Printing data`
-        p.classList.add("resultOutputTitle")
-        outputDocumentID.appendChild(p)  
-        // Prepare data to print
-        let cleanHotData = cleanHandosontableSourceData(hotDataArray,hotHeadersArray)
-        let cleanHotDataColwise = columnWiseHandsontableCleanData(cleanHotData)
-        let prettyPrintArray = JSON.stringify(cleanHotDataColwise,null,2)  
-        let p2 = document.createElement("p");
-        p2.innerHTML = prettyPrintArray
-        //outputDocumentID.appendChild(p2)
-        // Run descriptiveStatistics routine
-        let selectedCols = ["s_length", "s_width","p_length","p_width"]
-        let selectedStats = ["mean", "Variance"]
-        let desctStatsObjOutput = JSON.stringify(descriptiveStatistics(cleanHotDataColwise,selectedCols,selectedStats),null,2)
-        let p3 = document.createElement("p");
-        p3.innerHTML = desctStatsObjOutput
-        outputDocumentID.appendChild(p3)
+        p.innerHTML = clusterInfo
+        outputDocumentID.appendChild(p)
         // scroll to bottom
-        p2.scrollIntoView({behaviour:"smooth"});
+        p.scrollIntoView({behaviour:"smooth"});
     });
-    
-    //Run kmeans algorithm from kMeans.js and print it to Output
-    document.getElementById("computeKmeans").addEventListener("click",function(){
-        // Log operation
-        console.log("Runing kmeans")
-        // Get input options
-        let useNaiveShardingKmeans = false;
-        if( document.getElementById("kMeansInitializacion").value === "kMeansNaiveShardingInit"){
-            useNaiveShardingKmeans = true
-            console.log("using naive sharding")
-        }
-        // Get data from Handsontable
-        // Get data from Handsontable
-        let hotDataArray = hot.getSourceDataArray();
-        let hotHeadersArray = hot.getColHeader();
-        let dataKm = cleanHandosontableSourceData(hotDataArray,hotHeadersArray).arrOfArr
-        // Clean and validate data for kmeans algorithm
-        // PENDING
-        // Run kmeans algorithm
-        let k = Number(document.getElementById("kMeansClusters").value)
-        let result = kmeans(dataKm, k, useNaiveShardingKmeans)
-        // Print title and annotations in output region
-        let p = document.createElement("p");
-        p.innerHTML = `Kmeans clustering with ${k} clusters`
-        p.classList.add("resultOutputTitle")
-        outputDocumentID.appendChild(p)  
-        // Print result to output region
-        result.clusters.forEach(cluster => {
-            let clusterInfo = JSON.stringify(cluster.centroid,null,2) 
-            let p = document.createElement("p");
-            p.innerHTML = clusterInfo
-            outputDocumentID.appendChild(p)
-            // scroll to bottom
-            p.scrollIntoView({behaviour:"smooth"});
-        });
-    })
-    
-    // C) Layout functinalities
-    // Popupu modal implementation
-    // SEE https://medium.com/@GistCoding/simple-popup-modal-with-vanilla-javascript-a14515ec630b
-    const modalTriggers = document.querySelectorAll('.popup-trigger')
-    const modalCloseTrigger = document.querySelector('.popup-modal__close')
-    const bodyBlackout = document.querySelector('.body-blackout')
-    modalTriggers.forEach(trigger => {
-        trigger.addEventListener('click', () => {
-            const { popupTrigger } = trigger.dataset
-            const popupModal = document.querySelector(`[data-popup-modal="${popupTrigger}"]`)
-            popupModal.classList.add('is--visible')
-            bodyBlackout.classList.add('is-blacked-out')
-            
-            popupModal.querySelector('.popup-modal__close').addEventListener('click', () => {
-                popupModal.classList.remove('is--visible')
-                bodyBlackout.classList.remove('is-blacked-out')
-            })
-            
-            popupModal.querySelector('.popup-modal__run').addEventListener('click', () => {
-                popupModal.classList.remove('is--visible')
-                bodyBlackout.classList.remove('is-blacked-out')
-            })
-            
+})
+
+// C) Layout functinalities
+// Popupu modal implementation
+// SEE https://medium.com/@GistCoding/simple-popup-modal-with-vanilla-javascript-a14515ec630b
+const modalTriggers = document.querySelectorAll('.popup-trigger')
+const modalCloseTrigger = document.querySelector('.popup-modal__close')
+const bodyBlackout = document.querySelector('.body-blackout')
+modalTriggers.forEach(trigger => {
+    trigger.addEventListener('click', () => {
+        const { popupTrigger } = trigger.dataset
+        const popupModal = document.querySelector(`[data-popup-modal="${popupTrigger}"]`)
+        popupModal.classList.add('is--visible')
+        bodyBlackout.classList.add('is-blacked-out')
+        
+        popupModal.querySelector('.popup-modal__close').addEventListener('click', () => {
+            popupModal.classList.remove('is--visible')
+            bodyBlackout.classList.remove('is-blacked-out')
         })
+        
+        popupModal.querySelector('.popup-modal__run').addEventListener('click', () => {
+            popupModal.classList.remove('is--visible')
+            bodyBlackout.classList.remove('is-blacked-out')
+        })
+        
     })
-    
-}); // end of: DOMContentLoaded
+})
+
+//}); // end of: DOMContentLoaded
